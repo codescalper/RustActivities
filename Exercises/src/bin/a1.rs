833ed1536265ace78328afe96577d073de2c615b
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-09 06:31:05 +0000

    Did activity a4a on `match`

--- Exercises/src/bin/a1.rs
+++ Exercises/src/bin/a1.rs
@@ -2,10 +2,12 @@
 //
 // Program requirements:
 // * Displays your first and last name
-//
 
 // Notes:
 // * Use a function to display your first name
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
+fn main() {
+    print!("Hello");
+}
