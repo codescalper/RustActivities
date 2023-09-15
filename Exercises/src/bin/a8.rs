eaa0716c704e383a4c9098037ddf3c71127fa992
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-15 12:42:01 +0000

    did `struct` activity on a8

--- Exercises/src/bin/a8.rs
+++ Exercises/src/bin/a8.rs
@@ -12,9 +12,39 @@
 
 fn main() { 
 
-
-
-
-}
-
-
+    let orange_drink = Drinks{
+        flavor:Flavor::Orange,
+        fluid:12
+    };
+    let mango_drink = Drinks{
+        flavor:Flavor::Mango,
+        fluid:15
+    };
+    
+    print_drink_info(orange_drink);
+    print_drink_info(mango_drink);
+    
+    }
+    
+    enum Flavor{
+        Orange,
+        Mango,
+        Pineapple,
+        Guava
+    }
+    
+    struct Drinks{
+        flavor:Flavor,
+        fluid:i32
+    }
+    
+    fn print_drink_info(drink: Drinks) {
+        match drink.flavor { //Since match is exhaustive we will have to use all th testcases
+            Flavor::Orange => println!("Flavor: Orange, Fluid Ounces: {}", drink.fluid),
+            Flavor::Mango => println!("Flavor: Mango, Fluid Ounces: {}", drink.fluid),
+            Flavor::Pineapple => println!("Flavor: Pineapple, Fluid Ounces: {}", drink.fluid),
+            Flavor::Guava => println!("Flavor: Guava, Fluid Ounces: {}", drink.fluid),
+        }
+    }
+    
+    
