765a6053d5ca36110ed8503e5f7305245045491e
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-15 13:14:18 +0000

    Did activity on `tuple` at a9

--- Exercises/src/bin/a9.rs
+++ Exercises/src/bin/a9.rs
@@ -10,9 +10,24 @@
 // * Use an if..else if..else block to determine what to print
 
 
+fn main() {
 
-
-
-
-
-fn main() {}
+    //Now to destructure it i can create a variable and store the value os tuple in it 
+    
+        let (_x,y)=get_cartesian_coordinates();// _x because in rust if we don;t use a variable we can write a underscore to it    
+        if y>5{
+            println!("y is greater than 5");
+        }else if y<5{
+            println!("y is smaller than 5")
+        }else{
+            println!("y is equal to 5")
+        }
+    
+    }
+    
+    
+    
+    fn get_cartesian_coordinates() -> (i32, i32) {
+        // This function returns a tuple of two integers representing (x, y) coordinates.
+        (3, 5)
+    }
