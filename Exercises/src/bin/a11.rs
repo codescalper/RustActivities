d21df57c17a463d8b93d3087dc2aca7f3669a1be
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-17 11:28:05 +0000

    did activity on ownership on `a11`

--- Exercises/src/bin/a11.rs
+++ Exercises/src/bin/a11.rs
@@ -14,7 +14,27 @@
 
 fn main() {
 
+    let items =Grocery{
+        item:String::from("Tomato"),
+        id:3,
+        quantity:123
+    };
    
+    println!("The quantity of item is {}",display_quantity(&items));//If i would have passed the value without reference i could have not be able to use it in the next function call 
+    println!("The id of item is {}",display_id(&items)); //Referecne is borrowing the value not actually moving it 
 
 }
 
+struct Grocery{
+    item:String,
+    id:i32,
+    quantity:i32
+}
+
+fn display_quantity(grocery:&Grocery)->i32{ // Pasing the data type as reference struct 
+    grocery.quantity
+}
+
+fn display_id(grocery:&Grocery)->i32{ 
+    grocery.id
+}
