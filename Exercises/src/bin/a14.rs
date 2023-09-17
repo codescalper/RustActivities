f308e59b6291a9b14c5a4b9b61a671c75aff2daf
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-17 11:28:05 +0000

    did activity on ownership on `a11`

--- Exercises/src/bin/a14.rs
+++ Exercises/src/bin/a14.rs
@@ -11,4 +11,39 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+fn main() {
+    let mut persons_list = vec![
+        Person{
+            age:9,
+            name:"Mike".to_string(),
+            color:"Yellow".to_string()
+        },
+        Person{
+            age:29,
+            name:"Kylie".to_owned(),
+            color:String::from("Pink")
+        },
+        Person{
+            age:9,
+            name:"Kim".to_string(),
+            color:"White".to_string()
+        },
+    ];
+
+    for persons in persons_list{
+        if persons.age<10{
+            print_(&persons.name,&persons.color);
+        }
+    }
+}
+
+fn print_(name: &str, color: &str) {
+    println!("The color is {:?} and name is {:?}", color, name);
+}
+
+
+struct Person{
+    age:i32,
+    name:String,
+    color:String // we should pass the string as Owned string as struct beacuse STRUCT takes ownership of thier field
+}
