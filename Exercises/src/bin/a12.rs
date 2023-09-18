c355a49cf9274eba89f59944cb9b243087df1067
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-18 15:23:15 +0000

    did an amazing question on `impl`

--- Exercises/src/bin/a12.rs
+++ Exercises/src/bin/a12.rs
@@ -11,8 +11,58 @@
 // * Implement functionality on the box struct to print the characteristics
 
 
+fn main() {
+    let dim = Dimensions {
+        length: 11.2,
+        breadth: 22.3,
+        depth: 33.4,
+    };
 
+    let new_box = Box::new(dim, 65.4, Color::White);
 
+    new_box.print_data();
+}
 
+struct Box {
+    dimensions: Dimensions,
+    weight: f64,
+    color: Color, 
+}
 
-fn main() {}
+struct Dimensions {
+    length: f64,
+    breadth: f64,
+    depth: f64,
+}
+
+impl Box {
+    fn new(dim: Dimensions, w: f64, rang: Color) -> Self {
+        Self {
+            dimensions: dim,
+            weight: w,
+            color: rang, // rang is color in hindi
+        }
+    }
+
+    fn print_data(&self) {
+        let box_color = match self.color {
+            Color::White => "White",
+            Color::Black => "Black",
+            Color::Green => "Green",
+        };
+        println!(
+            "The dimensions are {} {} {}, the weight is {} and the color is {}",
+            self.dimensions.length,
+            self.dimensions.breadth,
+            self.dimensions.depth,
+            self.weight,
+            box_color
+        );
+    }
+}
+
+enum Color {
+    White,
+    Black,
+    Green,
+}
