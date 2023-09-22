ac7486ccc9d5046f97472445da9e83a8f95b7d7c
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-22 11:55:33 +0000

    did activity on `vec` at a13

--- Exercises/src/bin/a13.rs
+++ Exercises/src/bin/a13.rs
@@ -10,4 +10,15 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+fn main() {
+    let v = vec![10,20,30,40];
+
+    for i in &v{ //* used reference as the copy trait is not allowed in vector since they are stored in heap
+        match i{
+            30=>println!("{} -> thirty",i),
+            _=> println!("{}",i),
+        }
+    }
+
+    println!("The length of the vector is {}",&v.len());
+}
