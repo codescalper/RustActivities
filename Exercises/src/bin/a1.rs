a41a5260f55dac50aab7e2a7eb8cddfd5e8e7ec4
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-01 09:08:57 +0000

    did basic activity on `function` a1

--- Exercises/src/bin/a1.rs
+++ Exercises/src/bin/a1.rs
@@ -8,6 +8,14 @@
 // * Use a function to display your last name
 // * Use the println macro to display messages to the terminal
 
-fn main() {
-    print!("Hello");
+fn main(){
+    println!("My first name is {:?} and last name is {:?}",first_name("Mayank".to_string()),last_name("Singh".to_string()));
+}
+
+fn first_name(name:String)->String{
+    name
+}
+
+fn last_name(name:String)->String{
+    name
 }
