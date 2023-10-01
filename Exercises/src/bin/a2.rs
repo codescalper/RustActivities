260d3336ae76bfdc96ef67451d74692056ad3f4f
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-01 09:11:33 +0000

    did arithematic acitivity  on a2

--- Exercises/src/bin/a2.rs
+++ Exercises/src/bin/a2.rs
@@ -8,4 +8,10 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn main() {
+    println!("The sum of the two number is {:?}",sum(2,3));
+}
+
+fn sum(a:i32,b:i32)->i32{
+    a+b
+}
