731e9e982c4ebc0b88802e61877e38987167f101
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-01 09:41:08 +0000

    did another activity on `function` a1b

--- Exercises/src/bin/a1b.rs
+++ Exercises/src/bin/a1b.rs
@@ -2,4 +2,18 @@
 // write a function which returns a Multiplication of two numbers 
 // write a function which returns a division of two numbers 
 
-fn main() {}
+fn main() {
+    println!("The addition of two number is {}",add(2,3));
+    println!("The subraction of two number is {}",sub(2,3));
+    println!("The multiplication of two number is {}",mul(2,3));
+}
+
+fn add(a:i32,b:i32)->i32{
+    a+b
+}
+fn sub(a:i32,b:i32)->i32{
+    a-b
+}
+fn mul(a:i32,b:i32)->i32{
+    a*b
+}
