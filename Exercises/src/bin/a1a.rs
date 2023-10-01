76e336a76c806d918b988f8d36b169fab11cb8d0
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-01 09:35:57 +0000

    did `function` activity on a1a

--- Exercises/src/bin/a1a.rs
+++ Exercises/src/bin/a1a.rs
@@ -1,4 +1,14 @@
 // Write function which accepts borrowed string and print it, write your first and last name using two different functions
 
 
-fn main() {}
+fn main() {
+    println!("My first name is {:?} and last name is {:?}",first_name("Mayank"),last_name("Singh"));
+}
+
+
+fn first_name(name:&str)->&str{
+    name
+}
+fn last_name(name:&str)->&str{
+    name
+}
