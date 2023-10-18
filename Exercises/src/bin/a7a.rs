3193a49be0c8bc5a691686efb9fe3fc80a905aa1
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-18 14:19:17 +0000

    did activity on `enum` and `structs` on a7a

--- Exercises/src/bin/a7a.rs
+++ Exercises/src/bin/a7a.rs
@@ -6,9 +6,44 @@
 //price:i32
 
 
+#[derive(Debug)]
+enum Car{
+    Mercedes,
+    BMW,
+    Audi
+}
 
-fn main(){
+fn car_variant (word:&str)-> Option<Car> {
+    match word{
+        "Mercedes" =>Some(Car::Mercedes),
+        "BMW" =>Some(Car::BMW),
+        "Audi" =>Some(Car::Audi),   
+        _ =>None,
+    }
+}
+
+struct Details{
+    car:Car,
+    price:i32
     
 }
 
 
+fn main(){
+    
+    let car_name = "BMW";
+    let get_car_variant = car_variant(car_name);
+    
+    if let Some(car) = get_car_variant{
+        let car_details = Details{
+            car,
+            price:2500000
+        };
+        
+        println!("The car name is {:?} ",car_details.car);
+        println!("The car price is {:?} ",car_details.price);
+    }else{
+        println!("Car not found")
+    }
+    
+}
