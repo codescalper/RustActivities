e8fd0012adfa6b922a8fb9a5e42fa06ea936ebe1
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-19 11:34:25 +0000

    struct activity on a8a

--- Exercises/src/bin/a8a.rs
+++ Exercises/src/bin/a8a.rs
@@ -10,21 +10,80 @@
  6-Create a function which returns a new Shape 
  7-Create a function which takes Shape and prints the dimension of all shapes 
 */
+#[derive(Debug,Copy,Clone)] //Added this so that structs can be used multiple time without facing move error
+struct Shape{
+    square:Square,
+    rectangle:Rectangle
+}
 
+#[derive(Debug,Copy,Clone)]
+struct Square{
+    side:i32
+}
+
+#[derive(Debug,Copy,Clone)]
+struct Rectangle{
+    length:i32,
+    breadth:i32
+}
+
+fn new_rectangle (l:i32,b:i32) -> Rectangle{
+    let rect = Rectangle{
+        length:l,
+        breadth:b
+    };
+    
+    return rect
+}
+
+fn new_square (s:i32) -> Square{
+    let sq = Square{
+        side:s
+    };
+    
+    return sq
+}
+
+
+fn new_shape(sq:Square,rect:Rectangle) ->Shape{
+    let shape = Shape{
+        square:sq,
+        rectangle:rect
+    };
+    return shape
+}
+
+fn print_dimension(shape:Shape){
+    println!("The dimension of the square are {:?} the dimension of rectangle are{:?} {:?}",shape.square.side,shape.rectangle.length,shape.rectangle.breadth);
+}
+
+
+
+
+fn main(){
+
+    let print_square = new_square(12);
+    let print_rect = new_rectangle(10,12);
+    let sq = Square{
+        side:12
+    };
+    let rect = Rectangle{
+        length:12,
+        breadth:10
+    };
+    
+    let shape = Shape{
+        square : sq,
+        rectangle:rect
+    };
+    let print_shape = new_shape(sq,rect);
+    
+    
+    println!("{:?}",print_square);
+    println!("{:?}",print_rect);
+    println!("{:?}",print_shape);
+    
+    print_dimension(shape);
 
     
-
-
-
-
-fn main() {
-    
-
-} 
-
-
-
-
-
-
-
+}
--- Exercises/src/bin/a8b.rs
+++ Exercises/src/bin/a8b.rs
@@ -1,89 +1,18 @@
-// Topic: Getting Used to with struct
+//Topic: Getting used to with Struct 
+
 //
-//Requirements:
+//Requirements
+//
 /*
- 1-Define  struct Shape having  square:Square and rectangle:Rectangle 
- 2-Square has one field as side:i32
- 3-Rectangle has two length:i32 and width:i32
- 4-Create a function which returns a new Rectangle
- 5-Create a function which returns a new Square
- 6-Create a function which returns a new Shape 
- 7-Create a function which takes Shape and prints the dimension of all shapes 
+1- Define a struct Book having two fields author:Author and price:i32, 
+2- Define a struct Author having two fields one is name and stars, 
+3- you gonna purchase a book only if author has rating >= 4.9 stars .
+
 */
-#[derive(Debug,Copy,Clone)] //Added this so that structs can be used multiple time without facing move error
-struct Shape{
-    square:Square,
-    rectangle:Rectangle
+
+
+fn main() {
+
+
 }
 
-#[derive(Debug,Copy,Clone)]
-struct Square{
-    side:i32
-}
-
-#[derive(Debug,Copy,Clone)]
-struct Rectangle{
-    length:i32,
-    breadth:i32
-}
-
-fn new_rectangle (l:i32,b:i32) -> Rectangle{
-    let rect = Rectangle{
-        length:l,
-        breadth:b
-    };
-    
-    return rect
-}
-
-fn new_square (s:i32) -> Square{
-    let sq = Square{
-        side:s
-    };
-    
-    return sq
-}
-
-
-fn new_shape(sq:Square,rect:Rectangle) ->Shape{
-    let shape = Shape{
-        square:sq,
-        rectangle:rect
-    };
-    return shape
-}
-
-fn print_dimension(shape:Shape){
-    println!("The dimension of the square are {:?} the dimension of rectangle are{:?} {:?}",shape.square.side,shape.rectangle.length,shape.rectangle.breadth);
-}
-
-
-
-
-fn main(){
-
-    let print_square = new_square(12);
-    let print_rect = new_rectangle(10,12);
-    let sq = Square{
-        side:12
-    };
-    let rect = Rectangle{
-        length:12,
-        breadth:10
-    };
-    
-    let shape = Shape{
-        square : sq,
-        rectangle:rect
-    };
-    let print_shape = new_shape(sq,rect);
-    
-    
-    println!("{:?}",print_square);
-    println!("{:?}",print_rect);
-    println!("{:?}",print_shape);
-    
-    print_dimension(shape);
-
-    
-}
