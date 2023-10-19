501319819141123b772f70db499227cbcbf10793
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-19 12:09:41 +0000

    did `strcuct` activity on a8

--- Exercises/src/bin/a8b.rs
+++ Exercises/src/bin/a8b.rs
@@ -10,9 +10,56 @@
 
 */
 
+#[derive(Debug)]
+struct Book{
+    author:Author,
+    price:i32
+}
+
+
+#[derive(Debug)]
+struct Author{
+    name:String,
+    stars:f32
+}
+
+fn purchase_book(book:Book) {
+    if book.author.stars >=4.9 {
+        println!("Purchasing this book");
+        
+    }else{
+    println!("Not purchasing this book")
+        
+    }
+}
+
+
 
 fn main() {
-
+    
+    let author1 = Author{
+           name:"Mayank".to_string(),
+           stars:5.2
+    };
+    let author2 = Author{
+           name:"Elvish".to_string(),
+           stars:4.2
+    };
+    
+    let book1 = Book{
+      author:author1,
+        price:100
+    };
+    let book2 = Book{
+        author:author2,
+        price:200
+    };
+    
+    purchase_book(book1);
+    purchase_book(book2);
+    
+    
+    
 
 }
 
