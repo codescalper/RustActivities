6c6d4f02b1c991d28ff98b87c2cd3500d24a7401
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-10-19 12:35:23 +0000

    wokred with `tuple` in rust

--- Exercises/src/bin/a9a.rs
+++ Exercises/src/bin/a9a.rs
@@ -1,3 +1,1 @@
-// Create a function which accepts a tuple of two and returns a reverse of it , and print the result
-
-fn main() {}
+c
--- Exercises/src/bin/a9b.rs
+++ Exercises/src/bin/a9b.rs
@@ -1,6 +1,21 @@
 // Create struct Tuple and try to print the data 
 
-fn main(){
-    
+// Define a struct named Tuple
+struct Tuple {
+    x: i32,
+    y: f64,
 }
+
+fn main() {
+    // Create an instance of the Tuple struct
+    let my_tuple = Tuple {
+        x: 42,
+        y: 3.14,
+    };
+
+    // Access and print the data in the struct
+    println!("x: {}", my_tuple.x);
+    println!("y: {}", my_tuple.y);
+}
+
 
